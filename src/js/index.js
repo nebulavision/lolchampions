1d514fa22c4ab3d03e32915d634b82a5c8efb725
Bruno <brunodiazmartin5@gmail.com>
2024-12-01 00:16:30 +0000

    Added champion filter

--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,9 @@ document.getElementById('lol-image').addEventListener('click', () => {
     window.open('https://www.leagueoflegends.com/es-es/', '_blank');
 });
 
+// Añadimos el evento keyup para filtrar los campeones según el usuario teclee el nombre
+document.getElementById('lol-input').addEventListener('keyup', (event) => filterChampions(event.target.value.toLowerCase()));
+
 // Función asíncrona IIFE para obtener todos los campeones llamando a la API
 (async function fetchChampions() {
     await fetch('https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json')
@@ -34,35 +37,42 @@ document.getElementById('lol-image').addEventListener('click', () => {
         }
     );
 
-    // Una vez obtenido todos los campeones, mostramos en el html
+    // Una vez obtenido todos los campeones, mostramos en el DOM
     await showChampions();
 })()
 
+// Función asíncrona para mostrar los campeones
 async function showChampions(){
     // Recorremos el array de capeones
-        champions.forEach((champion, index) => {
-            // Creamos el elemento para la card
-            const card = document.createElement('article');
-            //card.setAttribute("index", index);
-            // Agregamos la clase card a la card
-            card.className = 'card';
-
-            // Cargamos dentro de la card el contenido html
-            // la imagen del campeon, así como su nombre y título
-            card.innerHTML = `
-                <img src="${champion.image}" alt="${champion.name}"/>
-                <div class="card-info">
-                    <h3>${champion.name}</h3>
-                    <p>${champion.title}</p>
-                </div>
-            `;
-
-            // Añadimos el evento click para llamar a la función showData
-            card.addEventListener('click', () => openModal(index));
+        champions.forEach(champion => {
+            const card = createChampionCard(champion)
 
             // Agregamos la card del campeon al contendor padre
             championContainer.appendChild(card);
         });
+}
+
+// Función para crear la tarjeta del campeón
+function createChampionCard(champion) {
+    // Creamos el elemento para la card
+    const card = document.createElement('article');
+    // Agregamos la clase card a la card
+    card.className = 'card';
+
+    // Cargamos dentro de la card el contenido DOM
+    // la imagen del campeon, así como su nombre y título
+    card.innerHTML = `
+        <img src="${champion.image}" alt="${champion.name}"/>
+        <div class="card-info">
+            <h3>${champion.name}</h3>
+            <p>${champion.title}</p>
+        </div>
+    `;
+
+    // Añadimos el evento click para llamar a la función showData
+    card.addEventListener('click', () => openModal(index));
+
+    return card;
 }
 
 // Función para mostrar el modal
@@ -131,4 +141,20 @@ function closeModal() {
         modal.style.display = 'none';
         modalContent.classList.remove('fade-out');
     }, 300);
+}
+
+function filterChampions(championName) {
+    // Filtramos los campeones cuyo nombre empiece por championName,
+    // si resulta vacia, por defecto en JS startsWith('') devuelve true
+    let filteredChampions = champions.filter((champion) => { return champion.name.toLowerCase().startsWith(championName)});
+
+    // Seleccionamos el contenedor de las cards y lo limpiamos
+    const container = document.getElementById('container');
+    container.innerHTML = '';
+
+    // Recorremos el array filtrado, creamos la tarjeta y añadimos al DOM 
+    filteredChampions.forEach(champion => {
+        const card = createChampionCard(champion);
+        container.appendChild(card);
+    });
 }
